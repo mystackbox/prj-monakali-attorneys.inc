5cafc517620a35bdf92ebc9e90a16ea537e7097d
Yingi <yingishirinda@gmail.com>
2024-10-05 17:51:37 +0200

    error fixing

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrl: './carousel.component.scss',
+//   providers: [
+//     { provide: CarouselConfig, useValue: { interval: 1500, noPause: false, showIndicators: true } }
+//  ],
 })
 export class CarouselComponent {
 

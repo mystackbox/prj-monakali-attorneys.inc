0ebecdc60c641dbd66c13256107bf19818b95c9f
Yingi <yingishirinda@gmail.com>
2024-10-15 16:21:07 +0200

    carousel ts file added

---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { CarouselConfig } from 'ngx-bootstrap/carousel';
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss',
+  providers: [
+      { provide: CarouselConfig, useValue: { 
+        interval: 7000, 
+        noPause: false, 
+        showIndicators: true,
+        isAnimated: true
+      } 
+    }
+ ],
+})
+export class CarouselComponent {
+
+  slides = [
+    {image: 'assets/carousel-img/5.jpg', title: 'We are a Law firm', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {image: 'assets/carousel-img/2.jpg', title: 'Reliable Services' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {image: 'assets/carousel-img/6.jpg', title: 'Quality Legal Services' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'},
+    {image: 'assets/carousel-img/1.jpg', title: 'Contact us today' , description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+ ];
+ noWrapSlides = false;
+ showIndicator = true;
+ constructor() { }
+
+ ngOnInit(): void {
+ }
+
+}
